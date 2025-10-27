8fb20ddace8fa7f8b238608dabe411e7aa0d62ce
Afonso Scheufele <martinischeufeleafonso@gmail.com>
2025-10-27 19:53:17 -0300

    Add: Rota /estoque/produtos para resolver 404

--- src/estoque/estoque-expanded.controller.ts
+++ src/estoque/estoque-expanded.controller.ts
@@ -1,9 +1,22 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { ProdutosModule } from '../produtos/produtos.module';
+import { ProdutosService } from '../produtos/produtos.service';
+import { Inject, forwardRef } from '@nestjs/common';
 
 @Controller()
 @UseGuards(JwtAuthGuard)
 export class EstoqueExpandedController {
+  constructor(
+    @Inject(forwardRef(() => ProdutosService))
+    private readonly produtosService: ProdutosService,
+  ) {}
+
+  @Get('estoque/produtos')
+  async listarProdutosEstoque() {
+    return await this.produtosService.findAll();
+  }
+
   @Get('estoque/transferencias')
   async listarTransferencias() {
     return [];
--- src/estoque/estoque-expanded.module.ts
+++ src/estoque/estoque-expanded.module.ts
@@ -1,7 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { EstoqueExpandedController } from './estoque-expanded.controller';
+import { ProdutosModule } from '../produtos/produtos.module';
 
 @Module({
+  imports: [forwardRef(() => ProdutosModule)],
   controllers: [EstoqueExpandedController],
 })
 export class EstoqueExpandedModule {}
